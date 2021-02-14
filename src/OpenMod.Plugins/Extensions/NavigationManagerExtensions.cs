f7c7ab9a54db896c5b36abfe3c1a41b34c418262
Andrii Vitiv <cyberandrii@gmail.com>
2021-02-14 17:55:54 +0200

    Add pagination

---
+++ src/OpenMod.Plugins/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace OpenMod.Plugins.Extensions
+{
+    public static class NavigationManagerExtensions
+    {
+        public static T? GetQuery<T>(this NavigationManager navigationManager, string key)
+        {
+            var result = GetQuery(navigationManager, typeof(T), key);
+            return result == default ? default : (T) result;
+        }
+
+        public static object? GetQuery(this NavigationManager navigationManager, Type type, string key)
+        {
+            var uri = navigationManager.ToAbsoluteUri(navigationManager.Uri);
+
+            if (QueryHelpers.ParseQuery(uri.Query).TryGetValue(key, out var query))
+            {
+                var converter = TypeDescriptor.GetConverter(type);
+                return converter.ConvertFrom((string) query);
+            }
+
+            return null;
+        }
+    }
+}
