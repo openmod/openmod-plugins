15e781bbac5f0ad34c224ba2b89fd1abc97b8e45
Andrii Vitiv <cyberandrii@gmail.com>
2021-02-15 02:48:47 +0200

    Show empty description instead of default

--- src/OpenMod.Plugins/Models/Plugin.cs
+++ src/OpenMod.Plugins/Models/Plugin.cs
@@ -15,19 +15,32 @@ namespace OpenMod.Plugins.Models
         public Plugin(SearchResult searchResult) : this(
             searchResult.PackageId,
             searchResult.Title,
-            searchResult.Description,
-            searchResult.Authors
-                .SelectMany(x => x.Split(',').Select(y => y.Trim()))
-                .ToList(),
+            FixDescription(searchResult.Description),
+            FixAuthors(searchResult.Authors),
             searchResult.ProjectUrl)
         {
         }
 
-        public bool IsOfficial => Id.StartsWith("OpenMod.", StringComparison.OrdinalIgnoreCase)
-                                  && Authors.Any(x => x == Id || x.Trim().Equals("openmod", StringComparison.OrdinalIgnoreCase));
+        public bool IsOfficial { get; }
+            = Id.StartsWith("OpenMod.", StringComparison.OrdinalIgnoreCase)
+              && Authors.Any(x => x == Id || x.Trim().Equals("OpenMod", StringComparison.OrdinalIgnoreCase));
 
-        public string CommandInstall => "openmod install " + Id;
+        public string CommandInstall { get; } = "openmod install " + Id;
 
-        public string NuGetUrl => "https://www.nuget.org/packages/" + Id;
+        public string NuGetUrl { get; } = "https://www.nuget.org/packages/" + Id;
+
+        private static string FixDescription(string description)
+        {
+            return description.Trim() == "Package Description" ? "" : description;
+        }
+
+        private static IReadOnlyList<string> FixAuthors(IReadOnlyList<string> authors)
+        {
+            return authors
+                .SelectMany(x => x
+                    .Split(',')
+                    .Select(y => y.Trim()))
+                .ToList();
+        }
     }
 }
