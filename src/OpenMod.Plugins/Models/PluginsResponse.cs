6fa27d5b0e93a3478f84de50510737badcd83f6f
Andrii Vitiv <cyberandrii@gmail.com>
2021-02-25 17:12:33 +0200

    Add blacklist. Closes #1

--- src/OpenMod.Plugins/Models/PluginsResponse.cs
+++ src/OpenMod.Plugins/Models/PluginsResponse.cs
@@ -1,24 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using BaGet.Protocol.Models;
+﻿using System.Collections.Generic;
 
 namespace OpenMod.Plugins.Models
 {
     public record PluginsResponse(long Total, IReadOnlyList<Plugin> Plugins)
     {
-        public PluginsResponse(SearchResponse searchResponse) : this(
-            searchResponse.TotalHits,
-            GetValidPlugins(searchResponse))
-        {
-        }
-
-        private static IReadOnlyList<Plugin> GetValidPlugins(SearchResponse searchResponse)
-        {
-            return searchResponse.Data
-                .Where(x => x.Tags.Contains("openmod-plugin", StringComparer.OrdinalIgnoreCase))
-                .Select(x => new Plugin(x))
-                .ToList();
-        }
     }
 }
--- src/OpenMod.Plugins/Services/PluginRepository.cs
+++ src/OpenMod.Plugins/Services/PluginRepository.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using BaGet.Protocol;
+using BaGet.Protocol.Models;
 using OpenMod.Plugins.Models;
 
 namespace OpenMod.Plugins.Services
@@ -9,6 +12,23 @@ namespace OpenMod.Plugins.Services
     public class PluginRepository : IPluginRepository
     {
         private readonly ISearchClient _searchClient;
+
+        private static readonly string[] _packageBlacklist =
+        {
+            // Trademark violations
+            "VaultPlugin",
+            "F.AntiCosmetics",
+            "F.ItemRestrictions",
+            "F.RustResources",
+            "F.Announcer",
+            "F.RestoreInventory",
+        };
+
+        private static readonly string[] _authorBlacklist =
+        {
+            // Trademark violations
+            "FPlugins",
+        };
 
         public PluginRepository()
         {
@@ -21,8 +41,38 @@ namespace OpenMod.Plugins.Services
         {
             query = query.Replace("tags:", "", StringComparison.OrdinalIgnoreCase);
             query += " Tags:\"openmod-plugin\"";
+
             var response = await _searchClient.SearchAsync(query, skip, take, includePrerelease);
-            return new PluginsResponse(response);
+
+            Filter(response, out IReadOnlyList<Plugin> plugins, out long total);
+            return new PluginsResponse(total, plugins);
+        }
+
+        private static void Filter(SearchResponse searchResponse, out IReadOnlyList<Plugin> plugins, out long total)
+        {
+            plugins = searchResponse.Data
+                .Where(x => !IsBlacklisted(x) && x.Tags.Contains("openmod-plugin", StringComparer.OrdinalIgnoreCase))
+                .Select(x => new Plugin(x))
+                .ToList();
+
+            total = searchResponse.TotalHits - (searchResponse.Data.Count - plugins.Count);
+        }
+
+        private static bool IsBlacklisted(SearchResult plugin)
+        {
+            if (_packageBlacklist
+                .Any(b => plugin.PackageId.Trim().Equals(b, StringComparison.OrdinalIgnoreCase)))
+            {
+                return true;
+            }
+
+            if (_authorBlacklist
+                .Any(b => plugin.Authors.Any(x => x.Contains(b, StringComparison.OrdinalIgnoreCase))))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
